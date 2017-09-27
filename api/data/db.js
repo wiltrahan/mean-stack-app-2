f8c581d283014c681029a63e4ceb5c2bf7009f6a
wiltrahan <twilorip@gmail.com>
2017-09-27 14:57:36 -0400

    section 3 lecture 27

--- api/data/db.js
+++ api/data/db.js
@@ -1,39 +1,39 @@
-var mongoose = require('mongoose');
-var dburl = 'mongodb://localhost:27017/meanhotel';
+var mongoose = require("mongoose");
+var dburl = "mongodb://localhost:27017/meanhotel";
 
 mongoose.connect(dburl, {
   useMongoClient: true
 });
 
-mongoose.connection.on('connected', function() {
-  console.log('Mongoose connnected to ' + dburl);
+mongoose.connection.on("connected", function() {
+  console.log("Mongoose connnected to " + dburl);
 });
-mongoose.connection.on('disconnected', function() {
-  console.log('Mongoose disconnnected');
+mongoose.connection.on("disconnected", function() {
+  console.log("Mongoose disconnnected");
 });
-mongoose.connection.on('error', function(err) {
-  console.log('Mongoose connnection error ' + err);
+mongoose.connection.on("error", function(err) {
+  console.log("Mongoose connnection error " + err);
 });
 
-process.on('SIGINT', function() {
+process.on("SIGINT", function() {
   mongoose.connection.close(function() {
-    console.log('Mongoose disconnected through app termination (SIGINT)');
+    console.log("Mongoose disconnected through app termination (SIGINT)");
     process.exit(0);
   });
 });
 
-process.on('SIGTERM', function() {
+process.on("SIGTERM", function() {
   mongoose.connection.close(function() {
-    console.log('Mongoose disconnected through app termination (SIGTERM)');
+    console.log("Mongoose disconnected through app termination (SIGTERM)");
     process.exit(0);
   });
 });
 
-process.once('SIGUSR2', function() {
+process.once("SIGUSR2", function() {
   mongoose.connection.close(function() {
-    console.log('Mongoose disconnected through app termination (SIGUSR2)');
-    process.kill(process.pid, 'SIGUSR2');
+    console.log("Mongoose disconnected through app termination (SIGUSR2)");
+    process.kill(process.pid, "SIGUSR2");
   });
 });
 
-require('./hotels.model.js');
+require("./hotels.model.js");
--- api/data/hotels.model.js
+++ api/data/hotels.model.js
@@ -1,5 +1,34 @@
 var mongoose = require("mongoose");
 
+var reviewSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: true
+  },
+  review: {
+    type: String,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+var roomSchema = new mongoose.Schema({
+  type: String,
+  number: Number,
+  description: String,
+  photos: [String],
+  price: Number
+});
+
 var hotelSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -14,7 +43,17 @@ var hotelSchema = new mongoose.Schema({
   services: [String],
   description: String,
   photos: [String],
-  currency: String
+  currency: String,
+  reviews: [reviewSchema],
+  rooms: [roomSchema],
+  location: {
+    address: String,
+    //always store coordinates Long first, Lat second
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    }
+  }
 });
 
-mongoose.model('Hotel', hotelSchema);
+mongoose.model("Hotel", hotelSchema);
