282c350ec4177e291766bee8674d4099699c6576
wiltrahan <twilorip@gmail.com>
2017-09-27 14:06:21 -0400

    section 2 lecture 23

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -48,12 +48,26 @@ module.exports.hotelsGetOne = function(req, res) {
 };
 
 module.exports.hotelsAddOne = function(req, res) {
-  console.log("POST new hotel");
   var db = dbconn.get();
   var collection = db.collection('hotels');
   var newHotel;
 
-  res
-    .status(200)
-    .json(req.body);
+  console.log("POST new hotel");
+
+  if(req.body && req.body.name && req.body.stars) {
+    newHotel = req.body;
+    newHotel.stars = parseInt(req.body.stars, 10);
+    collection.insertOne(newHotel, function(err, response) {
+      console.log(response);
+      console.log(response.ops);
+      res
+        .status(201)
+        .json(response.ops);
+    });
+  } else {
+    console.log("Data missing from body");
+    res
+      .status(400)
+      .json({ message : "Required data missing from body" });
+  }
 };
