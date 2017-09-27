e13607c3663baf0d075b104f75c505a6b192e33c
wiltrahan <twilorip@gmail.com>
2017-09-27 17:47:36 -0400

    section 3 lecture 28

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,11 +1,7 @@
-var dbconn = require("../data/dbconnection.js");
-var ObjectId = require('mongodb').ObjectId;
-var hotelData = require('../data/hotel-data.json');
+var mongoose = require("mongoose");
+var Hotel = mongoose.model("Hotel");
 
 module.exports.hotelsGetAll = function(req, res) {
-
-  var db = dbconn.get();
-  var collection = db.collection('hotels');
 
   var offset = 0;
   var count = 5;
@@ -18,29 +14,24 @@ module.exports.hotelsGetAll = function(req, res) {
     count = parseInt(req.query.count, 10);
   }
 
-  collection
+  Hotel
     .find()
     .skip(offset)
     .limit(count)
-    .toArray(function(err, docs) {
-      console.log("Found Hotels", docs);
+    .exec(function(err, hotels) {
+      console.log("Found hotels", hotels.length);
       res
-        .status(200)
-        .json(docs);
+        .json(hotels);
     });
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-  var db = dbconn.get();
-  var collection = db.collection('hotels');
-
   var hotelId = req.params.hotelId;
   console.log('GET hotelId', hotelId);
 
-  collection
-    .findOne({
-      _id : ObjectId(hotelId)
-    }, function(err, doc) {
+  Hotel
+    .findById(hotelId)
+    .exec(function(err, doc) {
       res
         .status(200)
         .json( doc );
