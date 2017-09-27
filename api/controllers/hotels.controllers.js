d26af3f0dd5cba6dcf7f3566dc41cd0251be6530
wiltrahan <twilorip@gmail.com>
2017-09-27 18:54:32 -0400

    section 3 lecture 31

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -28,8 +28,11 @@ var runGeoQuery = function(req, res) {
 };
 
 module.exports.hotelsGetAll = function(req, res) {
+
   var offset = 0;
   var count = 5;
+  var maxCount = 10;
+
 
   if (req.query && req.query.lat && req.query.lng) {
     runGeoQuery(req, res);
@@ -44,9 +47,38 @@ module.exports.hotelsGetAll = function(req, res) {
     count = parseInt(req.query.count, 10);
   }
 
-  Hotel.find().skip(offset).limit(count).exec(function(err, hotels) {
-    console.log("Found hotels", hotels.length);
-    res.json(hotels);
+  if (isNaN(offset) || isNaN(count)) {
+    res
+      .status(400)
+      .json({
+        "message" : "If supplied, count and offset should be numbers"
+      })
+    return;
+  }
+  if (count > maxCount) {
+    res
+      .status(400)
+      .json({
+        "message" : "Count limit of " + maxCount + " exceeded"
+      });
+      return;
+  }
+
+  Hotel
+    .find()
+    .skip(offset)
+    .limit(count)
+    .exec(function(err, hotels) {
+      if (err) {
+        console.log("Error finding hotels");
+        res
+          .status(500)
+          .json(err);
+      } else {
+        console.log("Found hotels", hotels.length);
+        res
+          .json(hotels);
+      }
   });
 };
 
@@ -54,8 +86,26 @@ module.exports.hotelsGetOne = function(req, res) {
   var hotelId = req.params.hotelId;
   console.log("GET hotelId", hotelId);
 
-  Hotel.findById(hotelId).exec(function(err, doc) {
-    res.status(200).json(doc);
+  Hotel
+    .findById(hotelId)
+    .exec(function(err, doc) {
+      var response = {
+        status : 200,
+        message : doc
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!doc) {
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found"
+        };
+      }
+      res
+        .status(response.status)
+        .json(response.message);
   });
 };
 
@@ -69,13 +119,18 @@ module.exports.hotelsAddOne = function(req, res) {
   if (req.body && req.body.name && req.body.stars) {
     newHotel = req.body;
     newHotel.stars = parseInt(req.body.stars, 10);
-    collection.insertOne(newHotel, function(err, response) {
+    collection
+      .insertOne(newHotel, function(err, response) {
       console.log(response);
       console.log(response.ops);
-      res.status(201).json(response.ops);
+      res
+        .status(201)
+        .json(response.ops);
     });
   } else {
     console.log("Data missing from body");
-    res.status(400).json({ message: "Required data missing from body" });
+    res
+      .status(400)
+      .json({ message: "Required data missing from body" });
   }
 };
--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -7,16 +7,30 @@ module.exports.reviewsGetAll = function(req, res) {
   console.log('GET hotelId', hotelId);
 
   Hotel
-    .findById(hotelId)
+    .findById(id)
     .select('reviews')
     .exec(function(err, doc) {
-      console.log('Returned Doc', doc);
+      var response = {
+        status : 200,
+        message : []
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!doc) {
+        console.log("Hotel id not found in database", id);
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found " + id
+        };
+      } else {
+        response.message = doc.reviews ? doc.reviews : [];
+      }
       res
-        .status(200)
-        .json( doc.reviews );
+        .status(response.status)
+        .json(response.message);
     });
-
-
 };
 
 //Single Review
@@ -29,11 +43,33 @@ module.exports.reviewsGetOne = function(req, res) {
     .findById(hotelId)
     .select('reviews')
     .exec(function(err, hotel) {
-      console.log('Returned hotel', hotel);
-      var review = hotel.reviews.id(reviewId);
+      var response = {
+        status : 200,
+        message : {}
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!hotel) {
+        console.log("Hotel id not found in database", id);
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found " + id
+        };
+      } else {
+        // Get the review
+        response.message = hotel.reviews.id(reviewId);
+        // If the review doesn't exist Mongoose returns null
+        if (!response.message) {
+          response.status = 404;
+          response.message = {
+            "message" : "Review ID not found " + reviewId
+          };
+        }
+      }
       res
-        .status(200)
-        .json( review );
+        .status(response.status)
+        .json(response.message);
     });
-
 };
