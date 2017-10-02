a1a7f099de7f6be0798fd332c33a69b0a0517e10
wiltrahan <twilorip@gmail.com>
2017-10-02 17:05:29 -0400

    section 4 lecture 45

---
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -0,0 +1,10 @@
+angular.module('meanhotel').controller('HotelsController', HotelsController);
+
+function HotelsController($http) {
+  var vm = this;
+  vm.title = "MEAN Hotel App";
+  $http.get('/api/hotels?count=10').then(function(response) {
+    // console.log(response);
+    vm.hotels = response.data;
+  });
+}
