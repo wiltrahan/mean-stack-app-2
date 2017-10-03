a4656b595447feea2971d77529beaaceee23e7d4
wiltrahan <twilorip@gmail.com>
2017-10-03 11:41:24 -0400

    section 4 lecture 47

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -5,5 +5,10 @@ function HotelController($routeParams, hotelDataFactory) {
   var id = $routeParams.id;
   hotelDataFactory.hotelDisplay(id).then(function(response) {
     vm.hotel = response.data;
+    vm.stars = _getStarRating(response.data.stars);
   });
+
+  function _getStarRating(stars) {
+    return new Array(stars);
+  }
 }
