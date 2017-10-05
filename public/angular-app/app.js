be1a80755256604646b815ecc402f450e375bb50
wiltrahan <twilorip@gmail.com>
2017-10-05 12:58:41 -0400

    section 5 lecture 51

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -11,7 +11,12 @@ function config($routeProvider) {
       templateUrl: 'angular-app/hotel-display/hotel.html',
       controller: HotelController,
       controllerAs: 'vm'
-    });
+    })
+    .when('/register', {
+      templateUrl: 'angular-app/register/register.html',
+      controller: RegisterController,
+      controllerAs: 'vm'
+    })
 }
 
 
